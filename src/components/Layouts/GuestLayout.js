5b56929444e1a93c1d54d014b9e85ccb49f56d9f
yuta-2001 <85932615+yuta-2001@users.noreply.github.com>
2022-12-31 22:48:44 +0900

    header/footers作成

--- src/components/Layouts/GuestLayout.js
+++ src/components/Layouts/GuestLayout.js
@@ -4,7 +4,7 @@ const GuestLayout = ({ children }) => {
     return (
         <div>
             <Head>
-                <title>Laravel</title>
+                <title>日記管理アプリ</title>
             </Head>
 
             <div className="font-sans text-gray-900 antialiased">
