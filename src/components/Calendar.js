e13aaa2bee39a295d2bdaede3b4687dbda32efc3
yuta-2001 <85932615+yuta-2001@users.noreply.github.com>
2023-01-02 17:04:08 +0900

    必要プラグインインストール

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,12 +1,21 @@
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import allLocales from '@fullcalendar/core/locales-all';
+import { useCallback } from 'react';
+import interactionPlugin, { DateClickArg } from '@fullcalendar/interaction';
 
 const Calendar = () => {
+	const handleDateClick = useCallback((DateClickArg) => {
+		alert(DateClickArg.dateStr);
+	}, []);
+
 	return (
 		<FullCalendar
-			plugins={[dayGridPlugin]}
+			plugins={[dayGridPlugin, interactionPlugin]}
+			locales={allLocales}
 			locale="ja"
 			initialEvents={[{ title: 'initial event', start: new Date() }]}
+			dateClick={handleDateClick}
 		/>
 	)
 }
