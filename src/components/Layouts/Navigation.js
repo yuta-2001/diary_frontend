b5bfdf1e50ec2c2b832c713d88d9720f4b94f61f
yuta-2001 <85932615+yuta-2001@users.noreply.github.com>
2023-01-02 16:25:40 +0900

    日本語化対応

--- src/components/Layouts/Navigation.js
+++ src/components/Layouts/Navigation.js
@@ -25,8 +25,11 @@ const Navigation = ({ user }) => {
                     <div className="flex">
                         {/* Logo */}
                         <div className="flex-shrink-0 flex items-center">
-                            <Link href="/dashboard">
-                                <ApplicationLogo className="block h-10 w-auto fill-current text-gray-600" />
+                            <Link href="/dashboard" className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0 fill-black">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="block w-8 h-10">
+                                    <path d="M96 0C43 0 0 43 0 96V416c0 53 43 96 96 96H384h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V384c17.7 0 32-14.3 32-32V32c0-17.7-14.3-32-32-32H384 96zm0 384H352v64H96c-17.7 0-32-14.3-32-32s14.3-32 32-32zm32-240c0-8.8 7.2-16 16-16H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16zm16 48H336c8.8 0 16 7.2 16 16s-7.2 16-16 16H144c-8.8 0-16-7.2-16-16s7.2-16 16-16z"/>
+                                </svg>
+                                <span className="ml-3 text-xl">Diary</span>
                             </Link>
                         </div>
 
@@ -65,7 +68,7 @@ const Navigation = ({ user }) => {
                             }>
                             {/* Authentication */}
                             <DropdownButton onClick={logout}>
-                                Logout
+                                ログアウト
                             </DropdownButton>
                         </Dropdown>
                     </div>
@@ -146,7 +149,7 @@ const Navigation = ({ user }) => {
                         <div className="mt-3 space-y-1">
                             {/* Authentication */}
                             <ResponsiveNavButton onClick={logout}>
-                                Logout
+                                ログアウト
                             </ResponsiveNavButton>
                         </div>
                     </div>
